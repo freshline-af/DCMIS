9f8f04fca4f7e765b8163e1c6ec40e8bd2773695
= <=>
2021-04-30 18:20:40 +0430

    Modify StaffLoginController to enhance login function

--- API/StaffLoginController.js
+++ API/StaffLoginController.js
@@ -1,14 +1,37 @@
-const staffSchema = require('../API/models/Staff');
+// Import staff schema to be able query staff collection
+const staffSchema = require("../API/models/Staff");
+// Import this module to hash password
+const bcrypt = require("bcryptjs");
 
 // fetch username & password to very them into db.staff whether they are existing
 const StaffCredentials = async (req, res) => {
-    const found = await staffSchema.findOne(req.body);
-    if (found) {
-        res.json(found)
-    } else {
-        res.json('Sorry, username or password wrong!');
+  await staffSchema.findOne(
+    {
+      username: req.body.username,
+    },
+    function(err, staff) {
+      if (err) throw err;
+      if (staff) {
+        bcrypt.compare(req.body.password, staff.password, function(
+          err,
+          result
+        ) {
+          if (err) {
+            throw err;
+          } else {
+            if (result) {
+              res.json("Success");
+            } else {
+              res.json("Fail");
+            }
+          }
+        });
+      } else {
+        res.json("Please first sign up into the system and then try again.");
+      }
     }
-}
+  );
+};
+
 // export it to be able to use
 module.exports = StaffCredentials;
-
