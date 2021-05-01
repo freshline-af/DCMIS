0bd4d66eafa86aa99c58e93d5a1e4f5180022cab
Basir <basir.developer@gmail.com>
2021-05-01 11:22:00 +0430

    added and finshed not found page funtionality

---
+++ src/views/NotFoundPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-container>
+        <v-row justify="center" align="center" class="mt-16  text-center">
+        <v-col  cols="12" md="12" lg="12" sm="12" xl="12">
+         <h1>OOPS</h1>
+        <h3>{{resource}} که شما دنبالش هستین در این جا موجود نمی باشد </h3>
+        <router-link  :to="{name: 'Home' }" > برگشت به صفحه داشبورد</router-link>
+        </v-col>
+    </v-row>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        props: {
+    resource: {
+      type: String,
+      requried: true
+    }
+  }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
