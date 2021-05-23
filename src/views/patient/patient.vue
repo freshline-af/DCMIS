a2af628d972fda0b50fbbae087531f12077e8b72
Basir <basir.developer@gmail.com>
2021-05-23 14:48:25 +0430

    design the patient to show  deatils

--- src/views/patient/patient.vue
+++ src/views/patient/patient.vue
@@ -50,6 +50,99 @@
             </v-card-text>
           </v-card>
         </v-dialog>
+        <!-- dialog for show patient deatials -->
+        <v-dialog v-model="showDialog" max-width="500px" fullscreen>
+          <v-app>
+            <v-app-bar app flat>
+              <strong class="pr-3">نشان دادن مشخصات</strong>
+              <v-spacer></v-spacer>
+              <v-btn class="pr-3" @click="closeShowDialog" icon
+                ><v-icon>mdi-close</v-icon></v-btn
+              >
+            </v-app-bar>
+            <v-main>
+              <v-container>
+                <v-row>
+                  <v-col class="mr-n10" cols="12" md="4" lg="4" xl="4" sm="6">
+                    <v-card>
+                      <v-card-title align="center">
+                        <v-avatar size="80" class="mr-1" color="blue">
+                          <v-img v-if="editedItem.image!=null"> </v-img>
+                          <span class="white--text" v-else>{{showname(editedItem.firstname)}}</span>
+                        </v-avatar>
+                      </v-card-title>
+                      <v-divider></v-divider>
+                      <v-card-text class="mt-2">
+                        <v-row>
+                          <v-col class="mt-n2">
+                            <v-text-field  readonly v-model="editedItem.firstname" label="اسم"></v-text-field>
+                          </v-col>
+                           <v-col class="mt-n2">
+                            <v-text-field readonly v-model="editedItem.lastname" label="تخلص"></v-text-field>
+                          </v-col>
+
+                        </v-row>
+                        <v-row>
+                          <v-col class="mt-n2">
+                            <v-text-field readonly v-model="editedItem.fathername" label="اسم پدر"></v-text-field>
+                          </v-col>
+                          <v-col class="mt-n2">
+                            <v-text-field readonly v-model="editedItem.marital_status" label="حالت مدنی"></v-text-field>
+                          </v-col>
+                        </v-row>
+                        <v-row>
+                          <v-col class="mt-n2">
+                            <v-text-field readonly v-model="editedItem.tazkira_id" label="شماره تذکره"></v-text-field>
+                          </v-col>
+                        </v-row>
+                        <v-row>
+                          <v-col class="mt-n2">
+                            <v-text-field readonly v-model="editedItem.address" label="آدرس"></v-text-field>
+                          </v-col>
+                        </v-row>
+                        <v-row>
+                          <v-col class="mt-n2">
+                            <v-text-field readonly v-model="editedItem.phone" label="شماره تماس"></v-text-field>
+                          </v-col>
+                        </v-row>
+                        <v-row>
+                          <v-col class="mt-n2">
+                            <v-text-field readonly v-model="editedItem.blood_group" label="گروپ خون"></v-text-field>
+                          </v-col>
+                          <v-col class="mt-n2">
+                            <v-text-field readonly v-model="editedItem.age" label="سن"></v-text-field>
+                          </v-col>
+                        </v-row>
+                        <v-row>
+                          <v-col class="mt-n2">
+                            <v-text-field readonly v-model="editedItem.occupation" label="شغل"></v-text-field>
+                          </v-col>
+                          <v-col class="mt-n2">
+                            <v-text-field readonly v-model="editedItem.sex" label="جنسیت"></v-text-field>
+                          </v-col>
+                        </v-row>
+                        
+                      </v-card-text>
+                    </v-card>
+                  </v-col>
+
+                  <v-col cols="12" md="8" lg="8" xl="8" sm="8">
+                    <v-card>
+                      <v-card-title>
+                        <h2>content card title</h2>
+                      </v-card-title>
+                      <v-card-text>
+                        <v-row>
+                          <h2>row content</h2>
+                        </v-row>
+                      </v-card-text>
+                    </v-card>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-main>
+          </v-app>
+        </v-dialog>
         <!-- delete dialog -->
         <v-dialog v-model="dialogDelete" max-width="500px">
           <v-card>
@@ -74,6 +167,9 @@
     <!-- the CURD button -->
 
     <template v-slot:item.actions="{ item }">
+      <v-btn icon class="mr-2" @click="showitem(item)">
+        <v-icon>mdi-text</v-icon>
+      </v-btn>
       <v-btn icon class="mr-2" @click="editItem(item)">
         <v-icon>mdi-pencil</v-icon>
       </v-btn>
@@ -96,31 +192,32 @@
 
 <script>
 import Register from "./registarPatient";
-import Store from "../../store/index"
+import Store from "../../store/index";
 export default {
   components: {
-    Register
+    Register,
   },
   data: () => ({
     dialog: false,
+    showDialog: false,
     dialogDelete: false,
     search: "",
     patients: [],
     footer: {
-      itemsPerPageText: "تعداد مریض در هر صفحه"
+      itemsPerPageText: "تعداد مریض در هر صفحه",
     },
     headers: [
       {
         text: "آی دی",
         align: "start",
         sortable: false,
-        value: "_id"
+        value: "_id",
       },
       { text: "اسم", value: "fitstname" },
       { text: "اسم پدر", value: "fathername" },
       { text: "شماره تماس", value: "phone" },
       { text: "نوعیت بیماری", value: "disease" },
-      { text: "Actions", value: "actions", sortable: false }
+      { text: "Actions", value: "actions", sortable: false },
     ],
     patients_list: [],
     editedIndex: -1,
@@ -134,14 +231,14 @@ export default {
       address: "",
       occupation: "",
       bloodGroup: "",
-      photoUrl: ""
-    }
+      photoUrl: "",
+    },
   }),
 
   computed: {
     formTitle() {
       return this.editedIndex === -1 ? "ثبت بیمار جدید" : "Edit Item";
-    }
+    },
   },
 
   watch: {
@@ -150,16 +247,16 @@ export default {
     },
     dialogDelete(val) {
       val || this.closeDelete();
-    }
+    },
   },
-  
- async beforeRouteEnter(routeTo,routeFrom,next){
- await Store.dispatch("patient/getListOfPatient");
-  next();
- },
+
+  async beforeRouteEnter(routeTo, routeFrom, next) {
+    await Store.dispatch("patient/getListOfPatient");
+    next();
+  },
 
   created() {
-  this.initialize();
+    this.initialize();
   },
   methods: {
     initialize() {
@@ -171,7 +268,18 @@ export default {
       this.editedItem = Object.assign({}, item);
       this.dialog = true;
     },
-
+    //  mthod for open the dialog to show the patient deatials
+    showitem(item) {
+      this.editedIndex = this.patients.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.showDialog = true;
+    },
+    // method for close the dialog of show patient deatils
+    closeShowDialog() {
+      this.editedIndex ="";
+      this.editedItem = "";
+      this.showDialog = false;
+    },
     deleteItem(item) {
       this.editedIndex = this.patients.indexOf(item);
       this.editedItem = Object.assign({}, item);
@@ -206,8 +314,13 @@ export default {
         this.desserts.push(this.editedItem);
       }
       this.close();
+    },
+    showname(firstname){
+           let name = ""+firstname+"".split('');
+           return name[0] + name[1]
+           //console.log(name.split(''))
     }
-  }
+  },
 };
 </script>
 
