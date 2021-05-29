3aacce982da15a03dba4401618c9b732e200e0b4
Basir <basir.developer@gmail.com>
2021-05-29 09:00:05 +0430

    Fixed app bar title to show the complate title

--- src/views/master-layout/app-bar/appBar.vue
+++ src/views/master-layout/app-bar/appBar.vue
@@ -4,7 +4,7 @@
     <leftDrawer :leftDrawer="leftDrawer"></leftDrawer>
     <v-app-bar app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-app-bar-title> سیستم مدریت کلینک دندان</v-app-bar-title>
+      <v-card-title> سیستم مدریت کلینک دندان</v-card-title>
       <v-spacer></v-spacer>
       <v-col cols="4" v-if="search">
         <v-text-field
