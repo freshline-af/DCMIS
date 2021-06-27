c5c37d9b9f17488132e7b0f4e72dc4a00d3b466b
Basir <basir.developer@gmail.com>
2021-06-27 23:11:20 +0430

    Modify patient design.

--- src/views/patient/registarPatient.vue
+++ src/views/patient/registarPatient.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
-    <v-form v-model="registarForm" ref="regis_patient" @submit.prevent="AddPatient" lazy-validation>
-      <v-row class="mt-3">
-        <v-col cols="6" md="6" sm="12" xs="12">
-          <v-row>
-            <v-col cols="12" md="12" sm="12" xs="12">
-              <h3>معلومات شخصی</h3>
-            </v-col>
-            <v-col cols="12">
+    <v-form
+      v-model="registarForm"
+      ref="regis_patient"
+      @submit.prevent="AddPatient"
+      lazy-validation
+    >
+      <v-row class="mt-3" justify="center">
+        <v-col align="center" cols="12" md="12" sm="12" xs="12">
+          <h3>معلومات شخصی</h3>
+        </v-col>
+        <v-col cols="12" align="center">
+          <v-row justify="center">
+            <v-col cols="12" md="6" lg="6" xl="6" sm="6">
               <v-divider></v-divider>
             </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="12" class="mt-3">
+          <v-row justify="center">
             <v-col cols="12" md="6" sm="12" xs="12">
               <BaseEdittext
                 label="اسم"
@@ -21,6 +30,10 @@
                 :rules="rule.rules.required_text"
               />
             </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="12" class="mt-n4">
+          <v-row justify="center">
             <v-col cols="12" md="6" sm="12" xs="12">
               <BaseEdittext
                 label="تخلص"
@@ -32,6 +45,10 @@
                 picon="mdi-text"
               />
             </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="12" class="mt-n4">
+          <v-row justify="center">
             <v-col cols="12" md="6" sm="12" xs="12">
               <BaseEdittext
                 label="نام پدر"
@@ -43,6 +60,10 @@
                 picon="mdi-text-short"
               />
             </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="12" class="mt-n4">
+          <v-row justify="center">
             <v-col cols="12" md="6" sm="12" xs="12">
               <BaseEdittext
                 label="سن"
@@ -54,7 +75,11 @@
                 picon="mdi-numeric-0"
               />
             </v-col>
-            <v-col cols="12" md="12" sm="12" xs="12">
+          </v-row>
+        </v-col>
+        <v-col cols="12" class="mt-n4">
+          <v-row justify="center">
+            <v-col cols="12" md="6" xl="6" lg="6" sm="12" xs="12">
               <BaseEdittext
                 label="نمبر تماس"
                 hint="وارد کردن شماره تماس الزامی می باشد"
@@ -65,7 +90,11 @@
                 picon="mdi-phone"
               />
             </v-col>
-            <v-col cols="12" md="12" sm="12" xs="12">
+          </v-row>
+        </v-col>
+        <v-col cols="12" class="mt-n4">
+          <v-row justify="center">
+            <v-col cols="12" md="6" lg="6" xl="6" sm="12" xs="12">
               <BaseEdittext
                 label="آدرس"
                 hint="وارد کردن آدرس الزامی میباشد"
@@ -76,88 +105,104 @@
                 picon="mdi-map-marker-outline"
               />
             </v-col>
-            <v-col cols="12" md="12" sm="12" xs="12">
-              <v-row>
-                <v-col>
-                  <BaseEdittext
-                    label="شغل"
-                    hint="وارد کردن شغل اختیاری میباشد"
-                    placeholder="لطفاشغل خود را وارد کنید"
-                    type="text"
-                    :rules="rule.rules.text"
-                    v-model="new_patient.occupation"
-                    picon="mdi-text-short"
-                  />
-                </v-col>
-                <v-col>
-                  <v-select
-                    label="جنسیت"
-                    :items="sex"
-                    outlined
-                    :rules="rule.rules.select"
-                    rounded
-                    required
-                    v-model="new_patient.sex"
-                  >
-                  </v-select>
-                </v-col>
-              </v-row>
+          </v-row>
+        </v-col>
+        <v-col cols="12" class="mt-n4">
+          <v-row justify="center">
+            <v-col cols="6" md="6" sm="12" xs="12">
+              <BaseEdittext
+                label="شغل"
+                hint="وارد کردن شغل اختیاری میباشد"
+                placeholder="لطفاشغل خود را وارد کنید"
+                type="text"
+                :rules="rule.rules.text"
+                v-model="new_patient.occupation"
+                picon="mdi-text-short"
+              />
             </v-col>
-            <v-col cols="12" md="12" sm="12" xs="12">
-              <v-row>
-                <v-col>
-                  <v-select
-                    label="گروپ خون"
-                    hint="وارد کردن گروپ خون الزامی می باشد میباشد"
-                    v-model="new_patient.blood_group"
-                    outlined
-                    rounded
-                    :rules="rule.rules.select"
-                    :items="blood_gropu"
-                    prepend-icon="mdi-blood-bag"
-                  />
-                </v-col>
-                <v-col>
-                  <v-select
-                    label="نوعیت مریضی"
-                    :items="services"
-                    v-model="new_patient.disease"
-                    :rules="rule.rules.select"
-                    outlined
-                    rounded
-                    required
-                  >
-                  </v-select>
-                </v-col>
-              </v-row>
+          </v-row>
+        </v-col>
+        <v-col cols="12" class="mt-n4">
+          <v-row justify="center">
+            <v-col cols="6" md="6" sm="12" xs="12">
+              <v-select
+                label="جنسیت"
+                :items="sex"
+                outlined
+                :rules="rule.rules.select"
+                rounded
+                required
+                v-model="new_patient.sex"
+              >
+              </v-select>
             </v-col>
-            <v-col cols="12" md="12" sm="12" xs="12">
-              <v-row>
-                <v-col>
-                  <v-select
-                  label="حالت مدنی"
-                  v-model="new_patient.marital_status"
-                  outlined
-                  rounded
-                  required
-                  :rules="rule.rules.select"
-                  :items="marital_status">
-
-                  </v-select>
-                </v-col>
-                <v-col>
-                  <BaseEdittext
-                  label="نمبر تذکره"
-                  v-model="new_patient.tazkira_id"
-                  placeholder="نمبر تذکره خود را وارد کنید"
-                  type="number"
-                  picon="mdi-number"
-                  :rules="rule.rules.number"
-                  />
-                </v-col>
-              </v-row>
+          </v-row>
+        </v-col>
+        <v-col cols="12" class="mt-n4">
+          <v-row justify="center">
+            <v-col cols="6" md="6" sm="12" xs="12">
+              <v-select
+                label="گروپ خون"
+                hint="وارد کردن گروپ خون الزامی می باشد میباشد"
+                v-model="new_patient.blood_group"
+                outlined
+                rounded
+                :rules="rule.rules.select"
+                :items="blood_gropu"
+                prepend-icon="mdi-blood-bag"
+              />
             </v-col>
-            <v-col cols="12" md="12" sm="12" xs="12">
+          </v-row>
+        </v-col>
+        <v-col cols="12" class="mt-n4">
+          <v-row justify="center">
+            <v-col cols="6" md="6" sm="12" xs="12">
+              <v-select
+                label="نوعیت مریضی"
+                :items="services"
+                v-model="new_patient.disease"
+                :rules="rule.rules.select"
+                outlined
+                rounded
+                required
+              >
+              </v-select>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="12" class="mt-n4">
+          <v-row justify="center">
+            <v-col cols="6" md="6" sm="12" xs="12">
+              <v-select
+                label="حالت مدنی"
+                v-model="new_patient.marital_status"
+                outlined
+                rounded
+                required
+                :rules="rule.rules.select"
+                :items="marital_status"
+              >
+              </v-select>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="12" class="mt-n4">
+          <v-row justify="center">
+            <v-col cols="6" md="6" sm="12" xs="12">
+              <BaseEdittext
+                label="نمبر تذکره"
+                v-model="new_patient.tazkira_id"
+                placeholder="نمبر تذکره خود را وارد کنید"
+                type="number"
+                picon="mdi-number"
+                :rules="rule.rules.number"
+              />
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="12" class="mt-n4">
+          <v-row justify="center">
+            <v-col cols="12" md="6" xl="6" lg="6" sm="12" xs="12">
               <v-file-input
                 :label="patient_label.photo"
                 hint="وارد کردن عکس اختیاری می باشد میباشد"
@@ -171,24 +216,36 @@
             </v-col>
           </v-row>
         </v-col>
-        <v-col cols="6" md="6" sm="12" xs="12">
-          <v-row class="mr-md-5">
-            <v-col cols="12">
-              <h3>هزینه</h3>
+            <v-col cols="12" >
+             <v-row justify="center">
+               <v-col align="center" cols="12" md="6" sm="12" lg="6">
+                  <h3>هزینه</h3>
+               </v-col>
+             </v-row>
             </v-col>
             <v-col cols="12">
-              <v-divider></v-divider>
+              <v-row justify="center">
+                <v-col cols="12" md="6" lg="6" sm="12">
+                  <v-divider></v-divider>
+                </v-col>
+              </v-row>
             </v-col>
-           <v-col>
+           <v-col cols="12">
+             <v-row justify="center">
+                <v-col cols="6" md="6" sm="12" xs="12">
               <BaseEdittext
-              label="مصارف کل"
-              v-model="fee.total_amount"
-              :rules="rule.rules.required_number"
-              type="number"
-              hint="وارد کردن مصرف الزامی می باشد"
-            />
+                label="مصارف کل"
+                v-model="fee.total_amount"
+                :rules="rule.rules.required_number"
+                type="number"
+                hint="وارد کردن مصرف الزامی می باشد"
+              />
+            </v-col>
+             </v-row>
            </v-col>
-            <v-col>
+            <v-col cols="12" class="mt-n4">
+              <v-row justify="center">
+                <v-col cols="6" md="6" sm="12" xs="12">
               <v-select
                 label="اقساط"
                 v-model="fee.installments"
@@ -199,30 +256,34 @@
               >
               </v-select>
             </v-col>
-          </v-row>
-          <v-row v-if="fee.installments !='تکمیل'" class="mr-md-5">
+              </v-row>
+            </v-col>
+          <v-col cols="12" class="mt-n4">
+             <v-row justify="center" v-if="fee.installments != 'تکمیل'" >
             <v-col cols="12" md="6">
               <BaseEdittext
-              label="مبلغ قابل پرداخت"
-              v-model="fee.payiad"
-              type="number"
-              :rules="rule.rules.number"
-              placeholder="مقدرا قابل پرداخت را وارد کنید"
-               />
+                label="مبلغ قابل پرداخت"
+                v-model="fee.payiad"
+                type="number"
+                :rules="rule.rules.number"
+                placeholder="مقدرا قابل پرداخت را وارد کنید"
+              />
             </v-col>
           </v-row>
-          <v-col>
-            <v-divider></v-divider>
           </v-col>
-          <v-row class="mr-md-5">
-            <v-col cols="12">
+
+            <v-col cols="12" align="center">
               <h3>تاریخچه مریض</h3>
             </v-col>
-            <v-col>
-              <v-divider></v-divider>
+            <v-col cols="12">
+             <v-row justify="center">
+               <v-col cols="12" md="6" sm="12">
+                  <v-divider></v-divider>
+               </v-col>
+             </v-row>
             </v-col>
             <v-col cols="12">
-              <v-row>
+              <v-row justify="center">
                 <v-col cols="12" md="4" sm="12">
                   <h3>{{ patient_caseHistory.heart }}</h3>
                 </v-col>
@@ -234,8 +295,8 @@
                 </v-radio-group>
               </v-row>
             </v-col>
-            <v-col cols="12">
-              <v-row>
+            <v-col cols="12" class="mt-n4">
+              <v-row justify="center">
                 <v-col cols="12" md="4" sm="12">
                   <h3 class="mt-2">{{ patient_caseHistory.dieabet }}</h3>
                 </v-col>
@@ -247,8 +308,8 @@
                 </v-radio-group>
               </v-row>
             </v-col>
-            <v-col cols="12">
-              <v-row>
+            <v-col cols="12" class="mt-n4">
+              <v-row justify="center">
                 <v-col cols="12" sm="12" md="4">
                   <h3>{{ patient_caseHistory.blood }}</h3>
                 </v-col>
@@ -271,8 +332,6 @@
               </v-btn>
             </v-col>
           </v-row>
-        </v-col>
-      </v-row>
       <v-divider class="mt-3"></v-divider>
       <v-row class="mt-3">
         <v-col class="text-left">
@@ -295,24 +354,21 @@ export default {
   },
   data() {
     return {
-      rule:rules,
+      rule: rules,
       registarForm: null,
       heart: "نخیر",
       dieabet: "نخیر",
       blood: "نخیر",
-      fee:[
+      fee: [
         {
-          total_amount:"",
+          total_amount: "",
           payiad: "",
-          installments:"",
-          date: ""
-        }
+          installments: "",
+          date: "",
+        },
       ],
       new_patient: {},
-      marital_status:[
-        "مجرد",
-        "متاهل"
-      ],
+      marital_status: ["مجرد", "متاهل"],
       sex: ["مذکر", "مونث"],
       blood_gropu: ["A+", "B+", "AB+", "O+", "A-", "B-", "AB-", "O-"],
       services: [
@@ -339,28 +395,27 @@ export default {
   },
   methods: {
     AddPatient() {
-      if(this.$refs.regis_patient.validate()){
+      if (this.$refs.regis_patient.validate()) {
         this.new_patient.case_history = [
-        {
-          disease: this.patient_caseHistory.heart,
-          result: this.heart,
-        },
-        {
-          disease: this.patient_caseHistory.blood,
-          result: this.blood,
-        },
-        {
-          disease: this.patient_caseHistory.dieabet,
-          result: this.dieabet,
-        },
-      ];
-      this.new_patient.fee = this.fee;
-      this.$store.dispatch("patient/addPatient", this.new_patient);
-      //this.$store.dispatch("patient/getListOfPatient")
-      this.$refs.regis_patient.reset();
-       //window.location.reload();
+          {
+            disease: this.patient_caseHistory.heart,
+            result: this.heart,
+          },
+          {
+            disease: this.patient_caseHistory.blood,
+            result: this.blood,
+          },
+          {
+            disease: this.patient_caseHistory.dieabet,
+            result: this.dieabet,
+          },
+        ];
+        this.new_patient.fee = this.fee;
+        this.$store.dispatch("patient/addPatient", this.new_patient);
+        //this.$store.dispatch("patient/getListOfPatient")
+        this.$refs.regis_patient.reset();
+        //window.location.reload();
       }
-     
     },
     skipCaseHistory() {
       (this.dieabet = "نخیر"), (this.blood = "نخیر"), (this.heart = "نخیر");
