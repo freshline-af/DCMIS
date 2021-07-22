27294cb89e61d3856d7961bf0ead83b59b23e1a7
Engineer Baqir Amini <baqir.amini01@gmail.com>
2021-07-22 20:38:08 +0430

    Complete API for retrieving all staff.

--- API/ReadStaffController.js
+++ API/ReadStaffController.js
@@ -1,6 +1,6 @@
 /* ----------------------------- Retrieve Staff -------------------- */
 // import model
-const Staff = require('./models/Staff');
+const Staff = require('./models/staff/Staff');
 const staffAll = async (req, res) => {
     const staff = await Staff.find().sort('-hired_at');
     res.json(staff);
